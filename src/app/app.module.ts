a2a9bc0111f6a158d30a68b3e6fc81659adbca53
greatfrank <lp_frank@163.com>
2019-09-16 02:20:23 +0800

    implement the angular reactivate form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
@@ -27,7 +27,8 @@ import { RegistComponent } from './regist/regist.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [{provide:LocationStrategy,useClass:HashLocationStrategy}],
   bootstrap: [AppComponent]
--- src/app/regist/regist.component.ts
+++ src/app/regist/regist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Teacher } from "../model/teacher";
 import { Student } from "../model/student";
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-regist',
@@ -9,15 +10,28 @@ import { Student } from "../model/student";
 })
 export class RegistComponent implements OnInit {
 
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  idPattern = "[0-9]*"
+
   teacher = new Teacher()
   student = new Student()
 
-  constructor(
-  ) { }
+  teacherForm = this.formBuilder.group({
+    id: ['', [Validators.required,Validators.pattern(this.idPattern)]],
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    department: ['', Validators.required],
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.teacher.department = '电子信息学院'
+  }
 
+  onSubmit(): void {
+    console.log(this.teacherForm.value);
   }
 
   registTeacher() {
